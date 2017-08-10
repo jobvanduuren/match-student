62c4d67d81555c013955e00fb838cbbb4d1a148d
jobvanduuren <job_van_duuren@hotmail.com>
2017-08-10 23:54:44 +0200

    add create student and show student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,27 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all.chronological
     @student_count = @students.count
+    @student = Student.new
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student_params = params.require(:student).permit(:name)
+
+    @student = Student.new(student_params)
+
+    if @student.save
+       redirect_to action: "index"
+    else
+       render 'new'
+    end
+  end
+
 end
